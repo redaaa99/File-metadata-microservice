f2d3e5b8f3c8c01b0106a076e157e6caaa103ad5
System <none>
2017-08-13 15:15:25 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -4,6 +4,8 @@
 // init project
 var express = require('express');
 var app = express();
+var multer = require("multer");
+var upload = multer({ dest: 'uploads/' })
 
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
@@ -16,8 +18,11 @@ app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
-app.post("/filesize", function (request, response) {
-  response.send("finished!");
+app.post("/filesize", upload.single('file'), function (req, res, next) {
+  console.log(req.file.size);
+  res.json({
+    
+  });
 });
 
 
